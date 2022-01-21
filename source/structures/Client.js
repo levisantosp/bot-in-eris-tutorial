bd4bbfd4f0a56bda9ca610b59c2cce17be2901ae
TiuShotto <levi.pires2005@gmail.com>
2022-01-21 17:42:59 -0300

    uma porra de coisa

--- source/structures/Client.js
+++ source/structures/Client.js
@@ -1,17 +1,33 @@
 const { Client, Collection } = require("eris")
 
 class App extends Client {
+    /**
+     * 
+     * @param {string} token 
+     * @param {string} options 
+     */
     constructor(token, options) {
         super(token, options)
         this.commands = new Collection()
         this.aliases = new Collection()
     }
 
+    /**
+     * 
+     * @param {string} path
+     * @param {string} name 
+     */
     async eLoad(path, name) {
         const event = new (require(`${path}/${name}`))(this)
         if (event.init) event.init(this)
         this.on(event.name, (...args) => event.run(...args))
     }
+
+    /**
+     * 
+     * @param {string} path 
+     * @param {string} name 
+     */
     async cLoad(path, name) {
         const cmd = new (require(`${path}/${name}`))(this)
         if (cmd.init) cmd.init(this)
