ed3e629d5c72e135f80116feb29179a6a8e0877f
Levi <levi.pires2005@gmail.com>
2022-06-01 21:58:21 -0300

    Episódio #7

---
+++ source/commands/slash/economy/leaderboard.js
@@ -0,0 +1,23 @@
+import Command from "../../../structures/Command.js"
+import User from "../../../database/models/User.js"
+import {Embed} from "eris-addons"
+
+export default class LeaderboardSlash extends Command {
+    constructor(client) {
+        super(client);
+        this.name = "leaderboard"
+        this.description = "Top 10 usuários com mais coins"
+    }
+    async run(interaction) {
+        const users = await User.find({coins: {$gt: 0}});
+        users.sort((a, b) => b.coins - a.coins);
+        const embed = new Embed();
+        embed.setTitle("Top 10 usuários com mais coins");
+        var a = 1;
+        for(const user of users) {
+            const u = await this.client.getRESTUser(user.id);
+            embed.addField(`${a++}° ${u.username}#${u.discriminator}`, `${user.coins.toLocaleString()} coins`);
+        }
+        interaction.createMessage(embed.build());
+    }
+}
